14975ecea1ef82ad877b4ceb7946a6733ff06797
William Santos <113859999+Williamwsm@users.noreply.github.com>
2024-04-12 21:07:52 -0300

    Update NoBuilder.java

--- ArvoreB_Builder/No/NoBuilder.java
+++ ArvoreB_Builder/No/NoBuilder.java
@@ -7,15 +7,11 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Data
 
 public class NoBuilder<T>{
     private NoBuilder<T> pai;
     private Integer elemento;
     private NoBuilder<T> esquerdo;
     private NoBuilder<T> direito;
-
-    @Override
-    public String toString() {
-        return  " " +elemento;
-    }
 }
